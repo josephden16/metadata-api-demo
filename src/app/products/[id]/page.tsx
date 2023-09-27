1f5fe90aa5da522b666d60718eaea3cdcc1c0d25
Denedo Oghenetega <tegadev01@gmail.com>
2023-09-27 11:14:18 +0100

    add products page and update the home page

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,54 @@
+import { Metadata } from "next";
+
+async function getProduct(id: string) {
+  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const data = await res.json();
+  return data;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const product = await getProduct(params.id);
+  return {
+    title: product.title,
+    description: product.description,
+    openGraph: {
+      title: product.title,
+      description: product.description,
+      images: [
+        {
+          url: product.image,
+        },
+      ],
+    },
+  };
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const product = await getProduct(params.id);
+  return (
+    <div>
+      <div className="mx-auto py-8">
+        <div className="max-w-lg mx-auto bg-white rounded-lg shadow-lg p-6">
+          <div className="mb-4">
+            <img
+              src={product.image}
+              alt="Product Image"
+              className="w-[200px] h-[200px] rounded-lg"
+            />
+          </div>
+          <h1 className="text-2xl font-semibold text-gray-800 mb-2">
+            {product.title}
+          </h1>
+          <p className="text-gray-600 mb-4">{product.description}</p>
+          <button className="bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 rounded-full transition duration-300 ease-in-out">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,45 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+async function fetchProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  return data;
+}
+
+export const metadta: Metadata = {
+  title: "Online Store | Products",
+  description: "Check out our products",
+  openGraph: {
+    title: "Online Store | Products",
+    description: "Check out our products",
+  },
+};
+
+export default async function Page() {
+  const products = await fetchProducts();
+  return (
+    <div className="p-5">
+      <h1 className="text-3xl font-bold mb-6">Products</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {products.map((product: any) => (
+          <div
+            key={product.id}
+            className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg"
+          >
+            <Link href={`/products/${product.id}`}>
+              <img
+                src={product.image}
+                alt={product.title}
+                className="w-full h-40 object-cover mb-2 rounded-lg"
+              />
+            </Link>
+            <h3 className="text-xl font-semibold">{product.title}</h3>
+            <p className="text-gray-500">{product.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
